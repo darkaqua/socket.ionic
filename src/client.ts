5897936a33f231c524cdeb776cf523bc91f551b2
pagoru <pagoru@gmail.com>
2024-07-01 17:23:57 +0200

    fixes

--- src/client.ts
+++ src/client.ts
@@ -44,58 +44,62 @@ export const getClientSocket = ({
 
   const connect = async (secure: boolean = false) =>
     new Promise((resolve, reject) => {
-      if (isConnected) return resolve(null);
-
-      !silent && reconnects === 0 && console.log(`Connecting to ${url}!`);
-      socket = new WebSocket(`${secure ? 'wss' : 'ws'}://${url}`, protocols);
-
-      // Connection opened
-      socket.addEventListener("open", () => {
-        isConnected = true;
-        !silent && console.log(`Connected to ${url}!`);
-        events.connected && events.connected();
-        resolve(null);
-        reconnects = 0;
-      });
-
-      // Listen for messages
-      socket.addEventListener("message", async ({ data }) => {
-        const { event, message, responseEventId } = JSON.parse(data);
-        if(!events[event]) return
+      try {
+        if (isConnected) return resolve(null);
         
-        const responseMessage = await events[event](message);
+        !silent && reconnects === 0 && console.log(`Connecting to ${url}!`);
+        socket = new WebSocket(`${secure ? 'wss' : 'ws'}://${url}`, protocols);
         
-        if(responseMessage)
-          socket.send(JSON.stringify({
-            event: `${event}#${responseEventId}`,
-            message: responseMessage
-          }));
-      });
-
-      socket.addEventListener("error", () => events.error && events.error());
-
-      socket.addEventListener("close", () => {
-        !silent && isConnected && console.log(`Disconnected from ${url}!`);
+        // Connection opened
+        socket.addEventListener("open", () => {
+          isConnected = true;
+          !silent && console.log(`Connected to ${url}!`);
+          events.connected && events.connected();
+          resolve(null);
+          reconnects = 0;
+        });
         
-        // resovle if it's already closed
-        if(isClosed) return resolve(null);
+        // Listen for messages
+        socket.addEventListener("message", async ({ data }) => {
+          const { event, message, responseEventId } = JSON.parse(data);
+          if(!events[event]) return
+          
+          const responseMessage = await events[event](message);
+          
+          if(responseMessage)
+            socket.send(JSON.stringify({
+              event: `${event}#${responseEventId}`,
+              message: responseMessage
+            }));
+        });
         
-        isConnected = false;
-        if (reconnect && reconnectIntents > reconnects) {
-          reconnects++;
-          !silent &&
+        socket.addEventListener("error", () => events.error && events.error());
+        
+        socket.addEventListener("close", () => {
+          !silent && isConnected && console.log(`Disconnected from ${url}!`);
+          
+          // resovle if it's already closed
+          if(isClosed) return resolve(null);
+          
+          isConnected = false;
+          if (reconnect && reconnectIntents > reconnects) {
+            reconnects++;
+            !silent &&
             console.log(
               `(${reconnects}/${reconnectIntents}) Reconnecting to ${url} in ${reconnectInterval}ms...`,
             );
-          setTimeout(async () => {
-            await connect();
-            resolve(null);
-          }, reconnectInterval);
-          return;
-        }
-        events.disconnected && events.disconnected();
-        resolve(null);
-      });
+            setTimeout(async () => {
+              await connect();
+              resolve(null);
+            }, reconnectInterval);
+            return;
+          }
+          events.disconnected && events.disconnected();
+          resolve(null);
+        });
+      } catch (e) {
+        reject(e)
+      }
     });
 
   const emit = (event: string, message?: any, response?: (message?: any) => void) => {
