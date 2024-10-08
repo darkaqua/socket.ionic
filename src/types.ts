58b6f150ec4b15f4a36f126da54bdeb2d642bb69
Pablo González Rubio <pagoru@gmail.com>
2024-10-08 17:49:36 +0200

    feat: jsr + conninfo on handler + types - fix #5 (#6)
    * feat: jsr + conninfo on handler + types - fix #5
    
    * feat: jsr + conninfo on handler + types - fix #5

--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,47 @@
-
-type EmitFunctionType = (event: string, data?: any, response?: (message?: any) => void) => void
+type EmitFunctionType = (
+  event: string,
+  data?: unknown,
+  response?: (message?: unknown) => void,
+) => void;
 
 export type ServerClient = {
   id: string;
   _emit: EmitFunctionType;
   emit: EmitFunctionType;
-  on: (event: string, callback: (data?: any) => Promise<void> | any) => void;
+  on: (
+    event: string,
+    callback: (data?: unknown) => Promise<void> | unknown,
+  ) => void;
   remove: (event: string, id: number) => void;
   rooms: string[];
 
   getRooms: () => Room[];
   addRoom: (name: string) => void;
   removeRoom: (name: string) => void;
-  
-  close: (code?: number, reason?: string) => void
-  
-  getSocket: () => WebSocket
+
+  close: (code?: number, reason?: string) => void;
+
+  getSocket: () => WebSocket;
 };
 
 export type Room = {
   name: string;
-  emit: (event: string, data?: any) => void;
+  emit: (event: string, data?: unknown) => void;
   clients: string[];
 
   getClients: () => ServerClient[];
   addClient: (clientId: string) => void;
   removeClient: (clientId: string) => void;
+};
+
+export type ServerSocket = {
+  on: (
+    event: "connected" | "disconnected" | "error" | "guest",
+    callback: (client: ServerClient, data?: unknown) => void,
+  ) => void;
+  emit: (event: string, data?: unknown) => void;
+  getClient: (clientId: string) => ServerClient;
+  getRoom: (name: string) => Room;
+  removeRoom: (name: string) => void;
+  close: () => Promise<void>;
 };
